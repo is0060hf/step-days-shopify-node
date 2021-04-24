292c817a768b084a74203fb2d9e32e6b91b145a0
is0060hf <kyuuya0358@gmail.com>
2021-04-24 14:10:04 +0900

    AdminAPIテスト接続

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+require('dotenv').config();
+
 var http = require('http');
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello World\n');
+
+    const Shopify = require('shopify-api-node');
+    const shopify = new Shopify({
+        shopName: process.env.SHOP_NAME,
+        accessToken: process.env.ACCESS_TOKEN,
+    });
+    shopify.order
+        .list({ limit: 5 })
+        .then((orders) => console.log(orders))
+        .catch((err) => console.error(err));
+
 }).listen(3000);
 
 console.log('Server running at http://127.0.0.1:3000');
